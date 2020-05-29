7bebbf2d9d57bf2a8572ce507ea7a266246275d7
James <tingjames2000@outlook.com>
2020-05-28 17:31:48 -0700

    Added colors to start and end nodes

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -1,47 +1,59 @@
-import React, {Component} from 'react';
-import Node from './Node/Node';
+import React, { Component } from "react";
+import Node from "./Node/Node";
 
-import './PathfindingVisualizer.css';
+import "./PathfindingVisualizer.css";
 
-const numRows = 15; //number of rows in the grid
+const numRows = 20; //number of rows in the grid
 const numCols = 50; // number of columns in the grid
 
-
 export default class PathfindingVisualizer extends Component {
-  
-    constructor (props) {
-        super(props);
-        this.state = {
-            nodes: [],
+  constructor(props) {
+    super(props);
+    this.state = {
+      nodes: [],
+    };
+  }
+
+  componentDidMount() {
+    const nodes = [];
+    for (let row = 0; row < numRows; row++) {
+      const currentRow = [];
+      for (let col = 0; col < numCols; col++) {
+        const currentNode = {
+          col,
+          row,
+          isStart: row === 10 && col === 5,
+          isFinish: row === 10 && col === 45,
         };
+        currentRow.push(currentNode);
+      }
+      nodes.push(currentRow);
     }
+    this.setState({ nodes });
+  }
 
-    componentDidMount() {
-        const nodes = []
-        for(let row = 0; row < numRows; row++) {
-            const currentRow = [];
-            for (let col = 0; col < numCols; col++) {
-                currentRow.push([]);
-            }
-            nodes.push(currentRow);
-        }
-        this.setState({nodes})
-    }
-
-
-    render () {
-        const {nodes} = this.state;
-        console.log(nodes)
-        return (
-            <div className = "grid">
-                {nodes.map((row, rowIndex) => {
-                    return <div> 
-                        {row.map((node,nodeIndex) => <Node></Node>)}
-                        </div>
-                        }
-                    )
-                }
+  render() {
+    const { nodes } = this.state;
+    console.log(nodes);
+    return (
+      <div className="grid">
+        {nodes.map((row, rowIndex) => {
+          return (
+            <div key={rowIndex}>
+              {row.map((node, nodeIndex) => {
+                const { isStart, isFinish } = node;
+                return (
+                  <Node
+                    key={nodeIndex}
+                    isStart={isStart}
+                    isFinish={isFinish}
+                  ></Node>
+                );
+              })}
             </div>
-        );
-    }
+          );
+        })}
+      </div>
+    );
+  }
 }
