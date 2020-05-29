869619b4ccceceb7aae333fff0d5f4a4f7421c12
James <tingjames2000@outlook.com>
2020-05-28 22:39:10 -0700

    Added Queue class

---
+++ src/Queue.js
@@ -0,0 +1,25 @@
+//custom queue class
+
+export class Queue {
+  constructor(type) {
+    this.items = [];
+  }
+
+  enqueue(element) {
+    this.items.push(element);
+  }
+
+  dequeue() {
+    if (this.isEmpty()) return "Underflow";
+    return this.items.shift();
+  }
+
+  front() {
+    if (this.isEmpty()) return "No elements in Queue";
+    return this.items[0];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+}
